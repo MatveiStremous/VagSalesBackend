6618b824977ba3e5628ce3f79bc8a85235dab07f
Matvei Stremous <matvei.stremous@gmail.com>
2023-04-07 10:42:55 +0300

    Initial commit

---
+++ src/main/java/com/example/vagsalesbackend/VagSalesBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.vagsalesbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VagSalesBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VagSalesBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/vagsalesbackend/VagSalesBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vagsalesbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VagSalesBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
