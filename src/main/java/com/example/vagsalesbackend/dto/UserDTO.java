cb3817b6139348e2d98f6beae0b00fee36d35536
Matvei Stremous <matvei.stremous@gmail.com>
2023-04-08 14:21:20 +0300

    refactor: add lombok annotations replaced constructors, getters and setters with lombok annotations add builder annotation

--- src/main/java/com/example/vagsalesbackend/dto/UserDTO.java
+++ src/main/java/com/example/vagsalesbackend/dto/UserDTO.java
@@ -1,40 +1,13 @@
 package com.example.vagsalesbackend.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
 public class UserDTO {
     private String email;
     private String password;
     private String name;
     private String phone;
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
 }
--- src/main/java/com/example/vagsalesbackend/models/User.java
+++ src/main/java/com/example/vagsalesbackend/models/User.java
@@ -1,9 +1,16 @@
 package com.example.vagsalesbackend.models;
 
+import lombok.*;
+
 import javax.persistence.*;
 
 @Entity
 @Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -15,72 +22,4 @@ public class User {
 
     @Enumerated(EnumType.STRING)
     private Role role;
-
-    public User() {
-    }
-
-    public User(String email, String password, String name, String phone, Role role) {
-        this.email = email;
-        this.password = password;
-        this.name = name;
-        this.phone = phone;
-        this.role = role;
-    }
-
-    public User(Integer id, String email, String password, String name, String phone, Role role) {
-        this.id = id;
-        this.email = email;
-        this.password = password;
-        this.name = name;
-        this.phone = phone;
-        this.role = role;
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
-    public Role getRole() {
-        return role;
-    }
-
-    public void setRole(Role role) {
-        this.role = role;
-    }
 }
